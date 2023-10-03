2bd5aaa71a7aa4171180861c5f2c4e94f39c5d92
Jtamara <josh.tamara91988@gmail.com>
2023-10-02 21:14:24 -0700

    added some backend stuff

--- login.sql
+++ login.sql
@@ -1,5 +1,5 @@
-Create database users;
-USE users;
+Create database login_system;
+USE login_system;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
