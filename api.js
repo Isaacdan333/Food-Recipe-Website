cc570dd6fe6d9c6fa01db14caa10084654c9142e
Ulysses Carbajal <112895957+Skippz07@users.noreply.github.com>
2023-10-02 19:46:52 -0700

    Update api.js, changed key, created recipecards

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 // API credentials
 const APP_ID = '072eb70b';
-const APP_KEY = 'd236403b4ba4a899b703964ff86deb6a	';
+const APP_KEY = 'db7277ebc3842274d74e4a4b9c002cae	';
 
 // DOM elements
 const searchForm = document.querySelector('#search-form');
@@ -11,24 +11,10 @@ const resultsSection = document.querySelector('#results');
 // API variables
 let searchQuery = '';
 let from = 0;
-let to = 50;
+let to = 20;
 let recipes = [];
 
 // Event listeners
-searchForm.addEventListener('submit', handleSearch);
-searchBtn.addEventListener('click', handleSearch);
-
-function handleSearch(event) {
-  event.preventDefault();
-  console.log('Search button clicked');
-  searchQuery = searchInput.value.trim();
-  if (searchQuery !== '') {
-    from = 0;
-    to = 50;
-    // Pass 'true' as the last argument to clear previous results
-    fetchRecipes(searchQuery, from, to, true);
-  }
-}
 
 // Fetch recipes
 async function fetchRecipes(query, from, to, clearResults) {
@@ -47,8 +33,6 @@ async function fetchRecipes(query, from, to, clearResults) {
     console.log(error);
   }
 }
-
-
 // Display recipes
 function displayRecipes(recipes) {
   if (recipes.length > 0) {
@@ -70,3 +54,65 @@ function displayRecipes(recipes) {
 }
 
 
+ window.addEventListener('click', function (e) {
+     if (e.target === resultsPage) {
+         resultsPage.style.display = 'none';
+     }
+ });
+ 
+ // Fetch recipes for different meal types
+ function fetchAndDisplayMeals() {
+     const breakfastQuery = getRandomQuery(['pancakes', 'omelette', 'cereal', 'toast']);
+     const lunchQuery = getRandomQuery(['sandwich', 'pasta', 'burger']);
+     const dinnerQuery = getRandomQuery(['steak', 'pizza', 'soup', 'stir fry']);
+     
+     fetchRecipes(breakfastQuery, 0, 30, true, 'column_1');
+     fetchRecipes(lunchQuery, 0, 30, true, 'column_2');
+     fetchRecipes(dinnerQuery, 0, 30, true, 'column_3');
+ }
+ 
+ // Function to get a random query from an array of queries
+ function getRandomQuery(queries) {
+     const randomIndex = Math.floor(Math.random() * queries.length);
+     return queries[randomIndex];
+ }
+ 
+ // Fetch recipes
+ async function fetchRecipes(query, from, to, clearResults, columnId) {
+     const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${from}&to=${to}`;
+     try {
+         const response = await fetch(url);
+         const data = await response.json();
+         const scrollableDiv = document.querySelector(`.${columnId} .scrollable`);
+         if (clearResults) {
+             scrollableDiv.innerHTML = ''; // Clear previous results
+         }
+         displayRecipes(data.hits, scrollableDiv); // Pass scrollableDiv to displayRecipes function
+     } catch (error) {
+         console.log(error);
+     }
+ }
+ 
+ function displayRecipes(recipes, resultsSection) {
+     if (recipes.length > 0) {
+         resultsSection.innerHTML = '';
+         recipes.forEach((recipe) => {
+             const recipeCard = `
+                 <div class="recipe">
+                     <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+                     <h2>${recipe.recipe.label}</h2>
+                     <p><strong>Calories:</strong> ${Math.round(recipe.recipe.calories)}</p>
+                     <button onclick="window.open('${recipe.recipe.url}', '_blank')">Get Recipe</button>
+                 </div>
+             `;
+             resultsSection.insertAdjacentHTML('beforeend', recipeCard);
+         });
+     } else {
+         resultsSection.innerHTML = '<p>No recipes found.</p>';
+     }
+ }
+ 
+ 
+ 
+ // Call fetchAndDisplayMeals function when the page loads
+ window.addEventListener('load', fetchAndDisplayMeals);
